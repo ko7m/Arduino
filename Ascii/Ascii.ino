d852376e269b5117f90c0c5ce4e7ae5ed06e170f
Jeff Whitlatch - ko7m <ko7m@arrl.net>
2017-04-29 15:47:25 -0700

    Cleaned up a bit

--- Ascii/Ascii.ino
+++ Ascii/Ascii.ino
@@ -7,9 +7,11 @@
 //
 // Copyright 2005 Jeff Whitlatch - ko7m
 
+#define count(x) (sizeof(x) / sizeof(x[0]))
+
 void setup()
 {
-	char *label[] = { ", dec: ", ", hex: ", ", oct: ", ", bin: "};
+	char *label[] = { ", dec: ", ", hex: ", ", oct: ", ", bin: " };
 	int conv[] = { DEC, HEX, OCT, BIN };
 
 	//Initialize serial and wait for port to open:
@@ -19,7 +21,7 @@ void setup()
 	for (int iChar = '!'; iChar <= '~'; iChar++)
 	{
 		Serial.write(iChar);
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < count(conv); i++)
 		{
 			Serial.print(label[i]);
 			Serial.print(iChar, conv[i]);
